e6af034b5c96789abc3e7e3b7e8ced7cd664fe1b
BAN <ggbbb@mineo.jp>
2022-01-05 01:13:07 +0900

    add updateScreenData

--- app/src/main/java/com/example/nesemu/view/ScreenView.kt
+++ app/src/main/java/com/example/nesemu/view/ScreenView.kt
@@ -12,15 +12,23 @@ class ScreenView(context: Context, attributeSet: AttributeSet) : View(context, a
     private val screenHeight : Int = 240;
     private val screen : Bitmap = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.ARGB_8888)
 
+    init {
+        val array = Array(256 * 240) {0xFF00FF00.toInt()}
+        screen.setPixels(array.toIntArray(), 0, screenWidth, 0, 0, screenWidth, screenHeight)
+    }
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         this.drawView(canvas)
     }
 
     private fun drawView(canvas: Canvas) {
-        val array = Array(256 * 240) {0xFF0000FF.toInt()}
-        screen.setPixels(array.toIntArray(), 0, screenWidth, 0, 0, screenWidth, screenHeight)
         val bmp = Bitmap.createScaledBitmap(screen, screenWidth * 4, screenHeight * 4, false);
         canvas.drawBitmap(bmp, 0f, 0f, Paint())
+    }
+
+    fun updateScreenData(data: Array<Int>) {
+        screen.setPixels(data.toIntArray(), 0, screenWidth, 0, 0, screenWidth, screenHeight)
+        invalidate()
     }
 }
