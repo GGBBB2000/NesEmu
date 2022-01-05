472f83abf800c99b71787899e9123b7fa8133009
BAN <ggbbb@mineo.jp>
2022-01-06 02:28:54 +0900

    impl DEX

--- app/src/main/java/com/example/nesemu/nes/cpu/instruction/DEX.kt
+++ app/src/main/java/com/example/nesemu/nes/cpu/instruction/DEX.kt
@@ -4,8 +4,10 @@ import android.location.Address
 import com.example.nesemu.nes.Bus
 import com.example.nesemu.nes.cpu.Register
 
-class DEX(x: Register.X, p: Register.P) : Instruction() {
+class DEX(val x: Register.X, val p: Register.P) : Instruction() {
     override fun exec() {
-        TODO("Not yet implemented")
+        x.value--
+        p.negative = x.value.toInt() < 0
+        p.zero = x.value.toInt() == 0
     }
 }
