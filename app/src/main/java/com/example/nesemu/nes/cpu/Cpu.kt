babd42d5d65175ec2d178dfe306db197153dc60f
BAN <ggbbb@mineo.jp>
2022-01-08 22:56:50 +0900

    impl CLC

--- app/src/main/java/com/example/nesemu/nes/cpu/Cpu.kt
+++ app/src/main/java/com/example/nesemu/nes/cpu/Cpu.kt
@@ -132,6 +132,7 @@ class Cpu(val bus: Bus, val nmi: NMI) : IODevice {
             //DEY
             0x88 -> InstructionInfo(0x88.toByte(), DEY(y, p), 2)
             //CLC
+            0x18 -> InstructionInfo(0x18.toByte(), CLC(p), 2)
             //SEC
             0x78 -> InstructionInfo(0x78, SEI(p), 2)
             //CLD
--- app/src/main/java/com/example/nesemu/nes/cpu/instruction/CLC.kt
+++ app/src/main/java/com/example/nesemu/nes/cpu/instruction/CLC.kt
@@ -2,8 +2,8 @@ package com.example.nesemu.nes.cpu.instruction
 
 import com.example.nesemu.nes.cpu.Register
 
-class CLC(p: Register.P) : Instruction() {
+class CLC(val p: Register.P) : Instruction() {
     override fun exec() {
-        TODO("Not yet implemented")
+        p.carry = false
     }
 }
