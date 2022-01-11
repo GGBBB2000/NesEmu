87dd1eb4df38411189e16ef3ad3dfb36088667c9
BAN <ggbbb@mineo.jp>
2022-01-12 01:09:04 +0900

    fix BNE
    fix to use proper flag for BNE

--- app/src/main/java/com/example/nesemu/nes/cpu/instruction/BNE.kt
+++ app/src/main/java/com/example/nesemu/nes/cpu/instruction/BNE.kt
@@ -1,11 +1,10 @@
 package com.example.nesemu.nes.cpu.instruction
 
-import android.location.Address
 import com.example.nesemu.nes.cpu.Register
 
 class BNE(val pc: Register.PC, val p: Register.P, val offset: Byte) : Instruction() {
     override fun exec() {
-        if (!p.negative) {
+        if (!p.zero) {
             pc.address += offset.toInt()
         }
     }
