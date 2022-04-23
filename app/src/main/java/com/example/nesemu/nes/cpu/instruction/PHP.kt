3b5d2d8adb9c038c2f7d50b8b4674a71707119c2
BAN <ggbbb@mineo.jp>
2022-04-23 13:59:15 +0900

    PHPの実装
    #3

--- app/src/main/java/com/example/nesemu/nes/cpu/instruction/PHP.kt
+++ app/src/main/java/com/example/nesemu/nes/cpu/instruction/PHP.kt
@@ -4,7 +4,5 @@ import com.example.nesemu.nes.Bus
 import com.example.nesemu.nes.cpu.Register
 
 class PHP(private val p: Register.P, private val sp: Register.SP, private val bus: Bus): Instruction() {
-    override fun exec() {
-        TODO("Not yet implemented")
-    }
+    override fun exec() = bus.write(sp.address--, p.toByte())
 }
