5d4ba563113960fafbcc470aa6f3fe8c4d88f8ad
BAN <ggbbb@mineo.jp>
2022-04-29 21:19:06 +0900

    引数の修正

--- app/src/main/java/com/example/nesemu/nes/cpu/instruction/ORA.kt
+++ app/src/main/java/com/example/nesemu/nes/cpu/instruction/ORA.kt
@@ -1,8 +1,12 @@
 package com.example.nesemu.nes.cpu.instruction
 
+import com.example.nesemu.nes.cpu.Argument
 import com.example.nesemu.nes.cpu.Register
 
-class ORA(data: Byte, a: Register.A, p: Register.P) : Instruction() {
+class ORA(arg: Argument.Readable, a: Register.A, p: Register.P) : Instruction() {
+    init {
+
+    }
     override fun exec() {
         TODO("Not yet implemented")
     }
