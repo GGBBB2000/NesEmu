6167ffa6515b95e8f773df2bf04c648a8cf6e51c
BAN <ggbbb@mineo.jp>
2022-04-29 15:51:14 +0900

    引数用クラスの実装
    #3

---
+++ app/src/main/java/com/example/nesemu/nes/cpu/Argument.kt
@@ -0,0 +1,125 @@
+package com.example.nesemu.nes.cpu
+
+import com.example.nesemu.nes.Bus
+import com.example.nesemu.nes.util.Address
+import java.util.*
+
+sealed class Argument {
+    interface Writable {
+        fun write(data: Byte)
+    }
+
+    interface Readable {
+        fun read() : Byte
+    }
+
+    interface ReadWritable: Readable, Writable
+
+    abstract override fun toString(): String
+
+    object Implied: Argument() {
+        override fun toString(): String = ""
+    }
+
+    class Accumulator(private val a: Register.A): ReadWritable, Argument() {
+        override fun read(): Byte = a.value
+        override fun write(data: Byte) {
+            a.value = data
+        }
+        override fun toString(): String = "A"
+    }
+
+    class Immediate(private val value: Byte): Readable, Argument() {
+        override fun read(): Byte = value
+        override fun toString(): String = "#$${(value.toInt() and 0xFF).toString(16)
+            .uppercase(Locale.getDefault())}"
+    }
+
+    class ZeroPage(immediateVal: Byte, private val bus: Bus): ReadWritable, Argument() {
+        private val address: Address = Address.buildAddress(0, immediateVal.toInt())
+        override fun read(): Byte = bus.read(address)
+        override fun write(data: Byte) = bus.write(address, data)
+        override fun toString(): String = "$${address}"
+    }
+
+    class ZeroPageX(immediateVal: Byte, private val xVal: Byte, private val bus: Bus)
+        : ReadWritable, Argument() {
+        private val address: Address = Address.buildAddress(0, immediateVal.toInt())
+        override fun read(): Byte = bus.read(address + xVal)
+        override fun write(data: Byte) = bus.write(address + xVal, data)
+        override fun toString(): String = "$${address},X"
+    }
+
+    class ZeroPageY(immediateVal: Byte, private val yVal: Byte, private val bus: Bus)
+        : ReadWritable, Argument() {
+        private val address: Address = Address.buildAddress(0, immediateVal.toInt())
+        override fun read(): Byte = bus.read(address + yVal)
+        override fun write(data: Byte) = bus.write(address + yVal, data)
+        override fun toString(): String = "$${address},Y"
+    }
+
+
+    class Absolute(val address: Address, private val bus: Bus)
+        : ReadWritable, Argument() {
+        override fun read(): Byte = bus.read(address)
+        override fun write(data: Byte) = bus.write(address, data)
+        override fun toString(): String = "$${address}"
+    }
+
+    class AbsoluteX(private val address: Address, private val xVal: Byte, private val bus: Bus)
+        : ReadWritable, Argument() {
+        override fun read(): Byte = bus.read(address + xVal)
+        override fun write(data: Byte) = bus.write(address + xVal, data)
+        override fun toString(): String = "$${address},X"
+    }
+
+    class AbsoluteY(private val address: Address, private val yVal: Byte, private val bus: Bus)
+        : ReadWritable, Argument() {
+        override fun read(): Byte = bus.read(address + yVal)
+        override fun write(data: Byte) = bus.write(address + yVal, data)
+        override fun toString(): String = "$${address},Y"
+    }
+
+    class Indirect(private val indirectAddress: Address, bus: Bus)
+        : Argument() {
+        val address: Address
+        init {
+            val lower = bus.read(indirectAddress)
+            val upper = bus.read(indirectAddress + 1)
+            address = Address.buildAddress(upper.toInt(), lower.toInt())
+        }
+
+        override fun toString(): String = "($${indirectAddress})"
+    }
+
+    class IndexedIndirect(private val base: Byte, x: Register.X, private val bus: Bus)
+        : ReadWritable, Argument() {
+        private val address: Address
+        init {
+            val indirectAddress = Address.buildAddress(0, base + x.value)
+            val lower = bus.read(indirectAddress)
+            val upper = bus.read(indirectAddress + 1)
+            address = Address.buildAddress(upper.toInt(), lower.toInt())
+        }
+
+        override fun write(data: Byte) = bus.write(address, data)
+        override fun read(): Byte = bus.read(address)
+        override fun toString(): String = "($${base.toString(16)}, X)"
+    }
+
+    class IndirectIndexed(private val base: Byte, y: Register.Y, private val bus: Bus)
+        : ReadWritable, Argument() {
+            private val address: Address
+            init {
+                val indirectAddress = Address.buildAddress(0, base.toInt())
+                val lower = bus.read(indirectAddress)
+                val upper = bus.read(indirectAddress + 1)
+                address = Address.buildAddress(upper.toInt(), lower.toInt()) + y.value
+            }
+
+        override fun write(data: Byte) = bus.write(address, data)
+        override fun read(): Byte = bus.read(address)
+        override fun toString(): String = "($${base.toString(16)}), Y"
+    }
+}
+
