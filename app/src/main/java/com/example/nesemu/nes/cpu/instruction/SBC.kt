8e41017ffeba4ccf50b7d4c558c939b6d49d66d2
BAN <ggbbb@mineo.jp>
2022-05-29 23:10:24 +0900

    SBC: 命令実装

--- app/src/main/java/com/example/nesemu/nes/cpu/instruction/SBC.kt
+++ app/src/main/java/com/example/nesemu/nes/cpu/instruction/SBC.kt
@@ -1,9 +1,20 @@
 package com.example.nesemu.nes.cpu.instruction
 
+import com.example.nesemu.nes.cpu.Argument
 import com.example.nesemu.nes.cpu.Register
 
-class SBC(data: Byte, a: Register.A, p: Register.P) : Instruction() {
+class SBC(private val arg: Argument.Readable,
+          private val a: Register.A,
+          private val p: Register.P) : Instruction() {
+
     override fun exec() {
-        TODO("Not yet implemented")
+        val input = (arg.read().toInt() + if (p.carry) 1 else 0)
+        val result = a.value.toInt() - input
+        p.negative = result.toByte() < 0
+        p.zero = (result and 0xFF) == 0
+        p.carry = a.value.toInt() < input
+        p.overflow = ((input < 0 && a.value > 0) && result >= 0) ||
+                ((input > 0 && a.value < 0) && result <= 0)
+        a.value = result.toByte()
     }
 }
